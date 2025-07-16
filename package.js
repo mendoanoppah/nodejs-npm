2d3bda44538a75d2862bf04a3c9c8345bbaee0c9
mendoanoppah <ayassshy43@gmail.com>
2025-07-16 08:29:46 +0700

    modified

---
+++ package.js
@@ -0,0 +1,13 @@
+import { sayHello, sum } from "ioo-npm";
+import { max, min } from "ioo-npm/number";
+
+// sayHello()
+console.info(sayHello("ioo"));
+
+// sum()
+console.info(sum([10, 10, 10, 10, 10]));
+
+// min()  
+console.info('hasil min: ' + min(5, 20));
+// max()
+console.info('hasil max: ' + max(19, 20));
